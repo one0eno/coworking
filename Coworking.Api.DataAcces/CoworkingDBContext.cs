cd839d964fd50eba5ae4fa609fc3476b98f3a265
jorge <jorge.arranz@gmail.com>
2021-11-25 23:46:41 +0100

    Añadimos cadena de conexion clase coworkingdbcontext e interfaz Icoworkingdbconetext, tambien se añaden paquetes nuget microsoft.entitiyframeworkcore.sqlServer a los proyectos de DataAcces

---
+++ Coworking.Api.DataAcces/CoworkingDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Coworking.Api.DataAcces.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coworking.Api.DataAcces
+{
+    public class CoworkingDBContext: DbContext ,ICoworkingDBContext
+    {
+
+        public CoworkingDBContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+        
+        
+        }
+    }
+}
--- Coworking.Api/Startup.cs
+++ Coworking.Api/Startup.cs
@@ -1,7 +1,9 @@
+using Coworking.Api.DataAcces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,7 @@ namespace Coworking.Api
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<CoworkingDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DataBaseConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
