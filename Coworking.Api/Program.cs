ae439bffc6a75ca88e09e8cc8d5c9640a6e39660
jorge <jorge.arranz@gmail.com>
2021-11-25 21:07:03 +0100

    First commit

---
+++ Coworking.Api/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coworking.Api
+{
+    public class Program
+    {
+
+
+        //esto es una prueba
+        public static void Main(string[] args)
+        {
+          
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
