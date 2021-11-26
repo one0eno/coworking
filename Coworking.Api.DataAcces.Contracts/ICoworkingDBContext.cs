79e5185e3fb6aa40aee90b92780d56d54daf07ab
jorge <jorge.arranz@gmail.com>
2021-11-26 17:38:45 +0100

    Configuracion dbcontext

--- Coworking.Api.DataAcces.Contracts/ICoworkingDBContext.cs
+++ Coworking.Api.DataAcces.Contracts/ICoworkingDBContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Coworking.Api.DataAcces.Contracts.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace Coworking.Api.DataAcces.Contracts
 {
     public interface ICoworkingDBContext
     {
+       
+       DbSet<UserEntity> Users { get; set; }
+       DbSet<AdminEntity> Admins { get; set; }
+       DbSet<BookingEntity> Bookings { get; set; }
+       DbSet<OfficeEntity> Offices { get; set; }
+       DbSet<Offices2RoomEntity> Offices2Rooms { get; set; }
+       DbSet<Room2ServicesEntity> Room2Services { get; set; }
+       DbSet<RoomEntity> Rooms { get; set; }
+       DbSet<ServicesEntity> Services { get; set; }
     }
 }
--- Coworking.Api.DataAcces/CoworkingDBContext.cs
+++ Coworking.Api.DataAcces/CoworkingDBContext.cs
@@ -1,4 +1,6 @@
 ﻿using Coworking.Api.DataAcces.Contracts;
+using Coworking.Api.DataAcces.Contracts.Entities;
+using Coworking.Api.DataAcces.EntityConfig;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,15 +12,38 @@ namespace Coworking.Api.DataAcces
 {
     public class CoworkingDBContext: DbContext ,ICoworkingDBContext
     {
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AdminEntity> Admins { get; set; }
+        public DbSet<BookingEntity> Bookings { get; set; }
+        public DbSet<OfficeEntity> Offices { get; set; }
+        public DbSet<Offices2RoomEntity> Offices2Rooms{ get; set; }
+        public DbSet<Room2ServicesEntity> Room2Services{ get; set; }
+        public DbSet<RoomEntity> Rooms{ get; set; }
+        public DbSet<ServicesEntity> Services { get; set; }
+
+        public CoworkingDBContext(DbContextOptions options) : base(options) 
+        {
+
+        }
 
         public CoworkingDBContext()
         {
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
-        
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            AdminEntityConfig.SetEntityBuilder(modelBuilder.Entity<AdminEntity>());
+            UserEntityConfig.SetEntityBuilder(modelBuilder.Entity<UserEntity>());
+            BookEntityConfig.SetEntityBuilder(modelBuilder.Entity<BookingEntity>());
+            OfficeEntityConfig.SetEntityBuilder(modelBuilder.Entity<OfficeEntity>());
+            Office2RoomEntityConfig.SetEntityBuilder(modelBuilder.Entity<Offices2RoomEntity>());
+            Room2ServiceEntityConfig.SetEntityBuilder(modelBuilder.Entity<Room2ServicesEntity>());
+            RoomEntityConfig.SetEntityBuilder(modelBuilder.Entity<RoomEntity>());
+            ServiceEntityConfig.SetEntityBuilder(modelBuilder.Entity<ServicesEntity>());
+
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
