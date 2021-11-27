b18b6524e4bf84fe76a6b0069e48359506425e49
jorge <jorge.arranz@gmail.com>
2021-11-27 01:32:49 +0100

    generamos primer controller

--- Coworking.Api.Bussines/Models/Admin.cs
+++ Coworking.Api.Bussines/Models/Admin.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Coworking.Api.Bussines.Models
 {
-    class Admin
+    public class Admin
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public int OfficeId { get; set; }
+        
+
     }
 }
---
+++ Coworking.Api/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Coworking.Api.Application.Contracts.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coworking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var name = await _adminService.GetAdminName(id);
+            return Ok(name);
+        }
+    }
+}
