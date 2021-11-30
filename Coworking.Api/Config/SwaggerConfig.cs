6d1ce34e242a4c715b3ba7cdb3c8af3c0b450c8f
jorge <jorge.arranz@gmail.com>
2021-11-30 17:24:27 +0100

    Configuracion de swagger

---
+++ Coworking.Api/Config/SwaggerConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coworking.Api.Config
+{
+    public static class SwaggerConfig
+    {
+
+        /// <summary>
+        /// Configuracion de swagger
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddRegistration(this IServiceCollection services)
+        {
+            Microsoft.OpenApi.Models.OpenApiInfo info = new Microsoft.OpenApi.Models.OpenApiInfo();
+            info.Title = "Coworking API";
+            info.Version = "V1";
+
+            string basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string xmlPath = Path.Combine(basePath, "Coworking.Api.xml");
+
+
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("V1", info);
+                    c.IncludeXmlComments(filePaht);
+
+                }
+
+            ) ;
+
+            return services;
+        }
+    }
+}
